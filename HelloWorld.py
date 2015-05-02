d7d06d3b22cdd882e6acfaa2dda3a461808a7068
Ding Zhou <dzhou@college.harvard.edu>
2015-05-02 16:56:10 -0400

    did alpha updating but no beta or normalizing

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,19 +4,20 @@ import example
 from sklearn import hmm
 NUM_STATES = 4
 NUM_OBSERVATIONS=4
-OBSERVATION_LENGTH=100000
+OBSERVATION_LENGTH=6
 np.random.seed(seed=1)
 
 
 def randomHMM():
-    A = np.random.rand(NUM_STATES, NUM_STATES)
-    # normalization from
-    # http://stackoverflow.com/questions/8904694/how-to-normalize-a-2-dimensional-numpy-array-in-python-less-verbose
-    A /= A.sum(axis=1)[:, np.newaxis]
-    B = np.random.uniform(0, 1, size=(NUM_STATES, NUM_OBSERVATIONS))
-    B /= B.sum(axis=1)[:, np.newaxis]
-    prior = [86] * NUM_STATES
-    return (A, B, prior)
+	A = np.random.rand(NUM_STATES, NUM_STATES)
+	# normalization from
+	# http://stackoverflow.com/questions/8904694/how-to-normalize-a-2-dimensional-numpy-array-in-python-less-verbose
+	A /= A.sum(axis=1)[:, np.newaxis]
+	B = np.random.uniform(0, 1, size=(NUM_STATES, NUM_OBSERVATIONS))
+	B /= B.sum(axis=1)[:, np.newaxis]
+	prior = np.ones(NUM_STATES)
+	prior /= prior.sum()
+	return (A, B, prior)
 
 def uniformHMM():
 	A = np.ones((NUM_STATES, NUM_STATES))
@@ -35,9 +36,9 @@ def uniformHMM():
 secretA, secretB, secretPrior = randomHMM()
 secretB = np.identity(NUM_STATES)
 
-#curState = np.random.choice(NUM_STATES, p=secretPrior)
+curState = np.random.choice(NUM_STATES, p=secretPrior)
 sequences = []
-for _ in range (0):
+for _ in range (1):
 	observationSequence = np.zeros(OBSERVATION_LENGTH)
 	for i in xrange(OBSERVATION_LENGTH):
 		observationSequence[i] = np.random.choice(NUM_OBSERVATIONS, p=secretB[curState, :])
@@ -56,11 +57,15 @@ def train(A, B, prior, observationSequence):
 				alphaTable[i, t] = prior[i]
 			else:
 				y_t = observationSequence[t-1]
-				alphaTable[i, t] = B[i, y_t] * np.dot(alphaTable[:, t-1], A[:, i])
+				alphaTable[i, t] = np.dot(alphaTable[:, t-1], A[:, i])#B[i, y_t] * np.dot(alphaTable[:, t-1], A[:, i])
 		# http://digital.cs.usu.edu/~cyan/CS7960/hmm-tutorial.pdf
 		# also based on other HMM small state space paper
+		#if t == 1:
+		#	return alphaTable[:,1]
+
 		normalizers[t] = sum(alphaTable[:, t])
-		alphaTable[:, t] /= normalizers[t]
+		#alphaTable[:, t] /= normalizers[t]
+	return alphaTable
 	betaTable = np.zeros((NUM_STATES, OBSERVATION_LENGTH+1))
 
 	for t in reversed(xrange(OBSERVATION_LENGTH+1)):
@@ -89,7 +94,7 @@ def train(A, B, prior, observationSequence):
 		alpha_k_sum = np.dot(alphaTable[:, t], betaTable[:, t]) 
 		for i in xrange(NUM_STATES):
 			for j in xrange(NUM_STATES):
- 				y_t1 = observationSequence[t]
+				y_t1 = observationSequence[t]
 				xiTable[i, j] += alphaTable[i, t] * A[i, j] * betaTable[j, t+1] * B[j, y_t1] / (alpha_k_sum * normalizers[t+1])
 
 
@@ -118,36 +123,45 @@ from graphlab import SGraph, Vertex, Edge
 
 
 def serial():
-    for observationSequence in sequences: 
-        print np.linalg.norm(A - secretA)
-        print np.linalg.norm(B - secretB)
-        print np.linalg.norm(prior - secretPrior)
-        for i in range(100):
-            print 'Iteration %d' %i	
-            A, B, prior =  train(A, B, prior, observationSequence)
-            print "A error: ", np.linalg.norm(A - secretA)
-            print "B error: ", np.linalg.norm(B - secretB)
-            print "prior error: ", np.linalg.norm(prior - secretPrior)
+	for observationSequence in sequences: 
+		print np.linalg.norm(A - secretA)
+		print np.linalg.norm(B - secretB)
+		print np.linalg.norm(prior - secretPrior)
+		for i in range(100):
+			print 'Iteration %d' %i	
+			A, B, prior =  train(A, B, prior, observationSequence)
+			print "A error: ", np.linalg.norm(A - secretA)
+			print "B error: ", np.linalg.norm(B - secretB)
+			print "prior error: ", np.linalg.norm(prior - secretPrior)
 
 
-def parallel():
+def parallel(observationSequence):
+	print "starting graph"
+	g = SGraph()
+	verticesEven = map(lambda i: Vertex(str(i) + " even", attr={'parity': 0, 'i': i, 'ait': [prior[i]] + ([0] * OBSERVATION_LENGTH), 'b': B[i, :]}), range(NUM_STATES))
+	verticesOdd = map(lambda i: Vertex(str(i) + " odd", attr={'parity': 1, 'i': i, 'ait': [0] * OBSERVATION_LENGTH, 'b': B[i, :]}), range(NUM_STATES))
 
-    g = SGraph()
-    verticesEven = map(lambda i: Vertex(str(i) + " odd", attr={'parity': -2, 'i': i, 'ait': prior[i], 'b': B[i, :]}), range(NUM_STATES ))
-    verticesOdd = map(lambda i: Vertex(str(i) + " even", attr={'parity': -1, 'i': i, 'ait': 0, 'b': B[i, :]}), range(NUM_STATES))
+	print "set up vertices, add vertices.."
+	g = g.add_vertices(verticesOdd + verticesEven)
+	print "finshed adding vertices..starting edge appending"
+	edges = []
+	for i in range (NUM_STATES):
+		for j in range (NUM_STATES):
+			edges.append(Edge(str(i) + " even", str(j) + " odd", attr={'parity': 0, 'aij': A[i, j]}))
+			edges.append(Edge(str(i) + " odd", str(j) + " even", attr={'parity': 1, 'aij': A[i, j]}))
 
-    g = g.add_vertices(verticesOdd + verticesEven)
 
-    edges = []
-    for i in range (NUM_STATES):
-        for j in range (NUM_STATES):
-            edges.append(Edge(str(i) + " even", str(j) + " odd", attr={'aij': A[i, j]}))
+	g = g.add_edges(edges)
 
-    g = g.add_edges(edges)
-    g = example.fp(g, B.tolist())
-    g.show()
-    import time
-    time.sleep(10000)
+	print "finished adding edges. calling example.fg..."
+	g = example.fp(g, observationSequence)
+	print "finished calling example fg"
+	print g.vertices
+	#g.show()
+	#import time
+	#time.sleep(10000)
 
-parallel()
+parallel(sequences[0])
+
+print train(A, B, prior, observationSequence)
 
--- example.cpp
+++ example.cpp
@@ -5,28 +5,46 @@
 
 using namespace graphlab;
 using namespace std;
+
+int t_iteration;
+
 void sum_shit(edge_triple& triple) {
-    triple.target["ait"] += triple.edge["aij"] * triple.source["ait"];
+    //triple.target["ait"] += triple.edge["aij"] * triple.source["ait"];
+
+   	if (t_iteration % 2 != (int)triple.edge["parity"]) {
+   		triple.target["ait"][t_iteration] += ((float)triple.source["ait"][t_iteration-1] * (float)triple.edge["aij"]);
+   	}
+    //triple.target["ait"][0] = 0;
+
 }
 
-gl_sgraph fp(gl_sgraph& g, std::vector<std::vector<float>> B) {
+gl_sgraph fp(gl_sgraph& g, std::vector<int> observation_seq) {
+
+
 
    // fill in Bi* for each vertex i
    // https://dato.com/products/create/sdk/docs/page_userguide_sframe.html
-    gl_sarray arr = g.vertices()["b"];
+    //gl_sarray arr = g.vertices()["b"];
     FILE *f = fopen("test.txt", "w");
+    /*
     for (std::vector<flexible_type> a: arr.range_iterator()) {
         for (float ai : a) {
             fprintf(f, "%f\n", ai);
         }
         fprintf(f, "\n");
     }
+	*/
+    //fclose(f);
+    for (t_iteration = 1; t_iteration < observation_seq.size() + 1; t_iteration++) {
+    	g = g.triple_apply(sum_shit, {"ait", "aij"});
+    	fprintf(f, "%d\n", t_iteration);
+    	fflush(f);
+	}
+	fclose(f);
 
-    fclose(f);
-
-    return g.triple_apply(sum_shit, {"ait", "aij"});
+	return g;
 }
 
 BEGIN_FUNCTION_REGISTRATION
-REGISTER_FUNCTION(fp, "g", "B"); // provide named parameters
+REGISTER_FUNCTION(fp, "g", "observation_seq"); // provide named parameters
 END_FUNCTION_REGISTRATION
