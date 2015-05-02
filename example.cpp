814dd9ea0c25c9b2a82c217100505b1d5878422f
Andy Shi <andyshi@college.harvard.edu>
2015-05-02 19:08:21 -0400

    added initial code for gammas

--- example.cpp
+++ example.cpp
@@ -2,6 +2,7 @@
 #include <graphlab/sdk/gl_sgraph.hpp>
 #include <iostream>
 #include<fstream>
+#include <assert.h>
 
 using namespace graphlab;
 using namespace std;
@@ -18,12 +19,51 @@ void sum_shit(edge_triple& triple) {
 
 }
 
+/*
+ * stuff andy wrote
+float vector_multiply(std::vector<flexible_type> a, 
+        std::vector<flexible_type> b) {
+
+    assert(a.size() == b.size());
+    std::vector<flexible_type> result;
+    result.resize(b.size());
+
+    for (int i = 0; i < a.size(); i++) {
+        result[i] = a[i] * b[i];
+    }
+
+    return result
+}
+
+float vector_divide(std::vector<flexible_type> a,
+        std::vector<flexible_type> b) {
+    assert(a.size() == b.size());
+    std::vector<flexible_type> result;
+    result.resize(b.size());
+
+    for (int i = 0; i < a.size(); i++) {
+        result[i] = a[i] / b[i];
+    }
+
+    return result
+}
+
+void get_gammas(gl_sgraph& g) {
+
+    verts = g.get_vertices();
+    gammas = verts.apply([](const std::vector<flexible_type>& x) { 
+                                    return vector_multiply(x[0], x[1]); 
+                                    }, flex_type_enum:FLOAT);
+    // column sums
+    //gammas_normalizer =  
+}
+
+*/
+
 gl_sgraph fp(gl_sgraph& g, std::vector<int> observation_seq) {
 
-
-
-   // fill in Bi* for each vertex i
-   // https://dato.com/products/create/sdk/docs/page_userguide_sframe.html
+    // fill in Bi* for each vertex i
+    // https://dato.com/products/create/sdk/docs/page_userguide_sframe.html
     //gl_sarray arr = g.vertices()["b"];
     FILE *f = fopen("test.txt", "w");
     /*
