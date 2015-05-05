a8ebe4f8328fd099564738feb05ac877c975a355
Andy Shi <andyshi@college.harvard.edu>
2015-05-05 16:26:43 -0400

    added new graph structure

---
+++ HelloWorld2.py
@@ -0,0 +1,172 @@
+import graphlab
+import numpy as np
+import example2
+from sklearn import hmm
+NUM_STATES = 5
+NUM_OBSERVATIONS=5
+OBSERVATION_LENGTH=40
+np.random.seed(seed=1)
+
+
+def randomHMM():
+	A = np.random.rand(NUM_STATES, NUM_STATES)
+	# normalization from
+	# http://stackoverflow.com/questions/8904694/how-to-normalize-a-2-dimensional-numpy-array-in-python-less-verbose
+	A /= A.sum(axis=1)[:, np.newaxis]
+	B = np.random.uniform(0, 1, size=(NUM_STATES, NUM_OBSERVATIONS))
+	B /= B.sum(axis=1)[:, np.newaxis]
+	prior = np.ones(NUM_STATES)
+	prior /= prior.sum()
+	return (A, B, prior)
+
+def uniformHMM():
+	A = np.ones((NUM_STATES, NUM_STATES))
+	# normalization from
+	# http://stackoverflow.com/questions/8904694/how-to-normalize-a-2-dimensional-numpy-array-in-python-less-verbose
+	A /= A.sum(axis=1)[:, np.newaxis]
+	B = np.ones((NUM_STATES, NUM_OBSERVATIONS))
+	#B = np.random.uniform(0, 1, size=(NUM_STATES, NUM_OBSERVATIONS))
+	B /= B.sum(axis=1)[:, np.newaxis]
+
+	prior = np.ones(NUM_STATES)
+	prior /= prior.sum()
+
+	return (A, B, prior)
+
+secretA, secretB, secretPrior = randomHMM()
+#secretB = np.identity(NUM_STATES)
+
+curState = np.random.choice(NUM_STATES, p=secretPrior)
+sequences = []
+for _ in range (1):
+	observationSequence = np.zeros(OBSERVATION_LENGTH)
+	for i in xrange(OBSERVATION_LENGTH):
+		observationSequence[i] = np.random.choice(NUM_OBSERVATIONS, p=secretB[curState, :])
+		curState = np.random.choice(NUM_STATES, p=secretA[curState, :])
+	sequences.append(observationSequence)
+
+def train(A, B, prior, observationSequence):
+	alphaTable = np.zeros((NUM_STATES, OBSERVATION_LENGTH +1))
+	# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.334.1331&rep=rep1&type=pdf
+	normalizers = np.zeros(OBSERVATION_LENGTH+1)
+
+	# compute forward probabilities
+	for t in xrange(OBSERVATION_LENGTH+1):
+		for i in xrange(NUM_STATES):
+			if t == 0:
+				alphaTable[i, t] = prior[i]
+			else:
+				y_t = observationSequence[t-1]
+				alphaTable[i, t] = B[i, y_t] * np.dot(alphaTable[:, t-1], A[:, i])
+		# http://digital.cs.usu.edu/~cyan/CS7960/hmm-tutorial.pdf
+		# also based on other HMM small state space paper
+
+		normalizers[t] = sum(alphaTable[:, t])
+		alphaTable[:, t] /= normalizers[t]
+
+	#print alphaTable
+	#return
+
+	betaTable = np.zeros((NUM_STATES, OBSERVATION_LENGTH+1))
+
+	for t in reversed(xrange(OBSERVATION_LENGTH+1)):
+		for i in xrange(NUM_STATES):
+			if t == OBSERVATION_LENGTH:
+				betaTable[i,t] = 1
+			else:
+				sm = 0
+				y_t = observationSequence[t]
+				for j in range(NUM_STATES):
+					sm += betaTable[j, t+1] * A[i, j] * B[j, y_t]	
+				betaTable[i,t] = sm
+
+		if t < OBSERVATION_LENGTH:
+			betaTable[:, t] /= normalizers[t+1]
+
+	print betaTable
+	return
+
+	gammaTable = np.zeros((NUM_STATES, OBSERVATION_LENGTH + 1))
+	for i in range (NUM_STATES):
+		for t in range(OBSERVATION_LENGTH + 1):
+			print "is this 1?", np.dot(alphaTable[:, t], betaTable[:, t])
+			gammaTable[i, t] = alphaTable[i, t] * betaTable[i, t] #/ np.dot(alphaTable[:, t], betaTable[:, t])
+	return None
+	newprior = gammaTable[:,0] # first column
+	gammaTable = gammaTable[:, 1:]
+
+	xiTable = np.zeros((NUM_STATES, NUM_STATES))
+	for t in range(OBSERVATION_LENGTH):
+		alpha_k_sum = np.dot(alphaTable[:, t], betaTable[:, t]) 
+		for i in xrange(NUM_STATES):
+			for j in xrange(NUM_STATES):
+				y_t1 = observationSequence[t]
+				xiTable[i, j] += alphaTable[i, t] * A[i, j] * betaTable[j, t+1] * B[j, y_t1] / (alpha_k_sum * normalizers[t+1])
+
+
+	newA = np.zeros((NUM_STATES, NUM_STATES))
+	for i in range(NUM_STATES):
+		for j in range(NUM_STATES):
+			newA[i, j] = xiTable[i, j] / (sum(gammaTable[i, :]))# - gammaTable[i, OBSERVATION_LENGTH-1])
+
+	newB = np.zeros((NUM_STATES, NUM_OBSERVATIONS))
+	for i in xrange(NUM_STATES):
+		for j in xrange(NUM_OBSERVATIONS):
+			s = 0
+			ss = 0
+			for t in xrange(OBSERVATION_LENGTH):
+				if j == observationSequence[t]:
+					s += gammaTable[i, t]
+				ss += gammaTable[i, t]	
+			newB[i, j] = s / ss	
+
+	return(newA, newB, newprior)
+
+A, B, prior = secretA, secretB, secretPrior
+
+from graphlab import SGraph, Vertex, Edge
+
+
+
+def serial():
+	for observationSequence in sequences: 
+		print np.linalg.norm(A - secretA)
+		print np.linalg.norm(B - secretB)
+		print np.linalg.norm(prior - secretPrior)
+		for i in range(100):
+			print 'Iteration %d' %i	
+			A, B, prior =  train(A, B, prior, observationSequence)
+			print "A error: ", np.linalg.norm(A - secretA)
+			print "B error: ", np.linalg.norm(B - secretB)
+			print "prior error: ", np.linalg.norm(prior - secretPrior)
+
+
+def parallel(A, B, prior, observationSequence):
+	print observationSequence
+	g = SGraph()
+
+	vertices = map(lambda i: Vertex(str(i), attr={'i': i, 'ait': [prior[i]] +
+		([0] * OBSERVATION_LENGTH), 'bit': ([0] * OBSERVATION_LENGTH) + [1],
+		'b': B[i, :], 'git': [0] * (OBSERVATION_LENGTH + 1)}),
+		xrange(NUM_STATES))
+
+	print "set up vertices, add vertices.."
+	g = g.add_vertices(vertices)
+	edges = []
+	for i in xrange(NUM_STATES):
+		for j in xrange(NUM_STATES):
+			edges.append(Edge(str(i), str(j), attr={'aij': A[i, j]}))
+
+	g = g.add_edges(edges)
+
+	print "finished adding edges. calling example.fg..."
+	g = example2.fp(g, observationSequence)
+	print "finished calling example fg"
+	print g.vertices
+	#g.show()
+	#import time
+	#time.sleep(10000)
+
+parallel(A, B, prior, sequences[0])
+print train(A, B, prior, observationSequence)
+
---
+++ example2.cpp
@@ -0,0 +1,103 @@
+#include <graphlab/sdk/toolkit_function_macros.hpp>
+#include <graphlab/sdk/gl_sgraph.hpp>
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+
+using namespace graphlab;
+using namespace std;
+
+std::vector<flexible_type> vector_multiply(std::vector<flexible_type> a, 
+        std::vector<flexible_type> b) {
+
+    assert(a.size() == b.size());
+    std::vector<flexible_type> result;
+    result.resize(b.size());
+
+    for (int i = 0; i < a.size(); i++) {
+        result[i] = a[i] * b[i];
+    }
+
+    return result;
+}
+
+std::vector<flexible_type> vector_divide(std::vector<flexible_type> a,
+        std::vector<float> b) {
+    assert(a.size() == b.size());
+    std::vector<flexible_type> result;
+    result.resize(b.size());
+
+    for (int i = 0; i < a.size(); i++) {
+        result[i] = a[i] / b[i];
+    }
+
+    return result;
+}
+
+void get_gammas(gl_sgraph& g) {
+
+}
+
+gl_sgraph fp(gl_sgraph& g, std::vector<int> observation_seq) {
+
+int t_iteration;
+int obseqt;
+float normalizer = 1;
+
+    std::vector<float> normalizers(observation_seq.size() + 1,1);
+
+    // compute alpha_i(t)
+    for (t_iteration = 1; t_iteration < observation_seq.size() + 1; t_iteration++) {
+        logprogress_stream << "Time goes by";
+        logprogress_stream << t_iteration;
+        logprogress_stream << observation_seq[t_iteration - 1];
+        // https://github.com/dato-code/GraphLab-Create-SDK/blob/master/sdk_example/sgraph_weighted_pagerank.cpp
+        // Normalization function
+        // based on documentation at https://dato.com/products/create/sdk/docs/classgraphlab_1_1gl__sframe.html
+        logprogress_stream << "Triple apply";
+        obseqt = observation_seq[t_iteration - 1]; 
+        // note ait is 1-indexed while observation sequence is 0 indexed
+     	g = g.triple_apply([t_iteration, obseqt, normalizer](edge_triple& triple) {
+            triple.target["ait"][t_iteration] += triple.target["b"][obseqt] * (((float)triple.source["ait"][t_iteration-1] / normalizer) * (float)triple.edge["aij"]);
+        }, {"ait", "aij", "b"});
+
+        logprogress_stream << "Triple apply done";
+
+        gl_sframe v = g.vertices();
+
+        normalizer = v["ait"].apply([t_iteration](const std::vector<flexible_type>& x) { 
+            return (float)x[t_iteration];
+        }, flex_type_enum::FLOAT).sum();
+        normalizers[t_iteration] = normalizer;
+
+	}
+    logprogress_stream << "Time goes by so slowly";
+	logprogress_stream << normalizers;
+
+	// calculate beta_i(t)
+    for (t_iteration = observation_seq.size() -1; t_iteration >= 0; t_iteration--) {
+        logprogress_stream << t_iteration;
+        // https://github.com/dato-code/GraphLab-Create-SDK/blob/master/sdk_example/sgraph_weighted_pagerank.cpp
+        // Normalization function
+        // based on documentation at https://dato.com/products/create/sdk/docs/classgraphlab_1_1gl__sframe.html
+        
+        obseqt = observation_seq[t_iteration];
+        //float normalizer = normalizers[t_iteration + 1];
+		float normalizer = 1;
+        logprogress_stream << normalizers[t_iteration + 1];
+        // Ding, note bit is 1-indexed while observation sequence is 0 indexed. 
+     	g = g.triple_apply([t_iteration, obseqt, normalizer](edge_triple& triple) {
+			triple.source["bit"][t_iteration] += triple.target["b"][obseqt] * (((float)triple.target["bit"][t_iteration+1]) * (float)triple.edge["aij"]) / normalizer;
+        }, {"bit", "aij", "b"});
+	}
+    
+    g.vertices()["git"] = g.vertices()[{"ait", "bit"}].apply([normalizers](const std::vector<flexible_type>& x) { 
+        return vector_divide(vector_multiply(x[0], x[1]), normalizers); 
+    }, flex_type_enum::LIST);
+
+    return g;
+}
+
+BEGIN_FUNCTION_REGISTRATION
+REGISTER_FUNCTION(fp, "g", "observation_seq"); // provide named parameters
+END_FUNCTION_REGISTRATION
