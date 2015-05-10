fecb3d8a9026d30ebf9e3197f7778061e811520f
Andy Shi <andyshi@college.harvard.edu>
2015-05-10 16:58:47 -0400

    updated gather, updated data

--- analysis/gather.sh
+++ analysis/gather.sh
@@ -1,3 +1,4 @@
 # gather all results from output files to csv
 tail --lines 1 *.out > fullout.csv
-tail --quiet --lines 1 *.out > data.csv
+echo "NUM_STATES,NUM_OBS,OBS_LEN,SERIAL,PARALLEL" > data.csv
+tail --quiet --lines 1 *.out >> data.csv
